447c8b1572268ca1f071d1606c785388cc91ca94
kahoau <kahoa827@gmail.com>
2021-09-06 16:01:45 +0800

    calculator styling

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -5,27 +5,27 @@
         <b-container>
           <b-row>
             <b-col>
-              <b-form-group
-                      id="sbutton-digits-label"
-                      label="Digits"
-                      label-for="sbutton-digits">
-                <b-form-spinbutton id="sbutton-decimal" min="1" max="3" v-model="digitsValue" placeholder="digits" inline></b-form-spinbutton>
-              </b-form-group>
-              <b-form-group
-                      id="sbutton-decimal-label"
-                      label="Decimal"
-                      label-for="sbutton-decimal">
-                <b-form-spinbutton id="sbutton-decimal" min="0" max="3" v-model="decimalValue" placeholder="decimal" inline></b-form-spinbutton>
-              </b-form-group>
+              Sign <b-form-select v-model="selectedCalculatorSign" :options="options"></b-form-select>
             </b-col>
           </b-row>
-          <b-row><b-col>&nbsp;</b-col></b-row>
           <b-row>
             <b-col>
-              <h4>{{ num1 }} - {{ num2 }} = {{answer}}</h4>
+                Digits <b-form-spinbutton id="sbutton-decimal" min="1" max="3" v-model="digitsValue" placeholder="digits" inline></b-form-spinbutton>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col>
+                Decimal <b-form-spinbutton id="sbutton-decimal" min="0" max="3" v-model="decimalValue" placeholder="decimal" inline></b-form-spinbutton>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col>
+              <b-jumbotron lead="Formula" text-variant="dark">
+                <h6>{{ num1 }} {{calculatorSign}} {{ num2 }} = {{answer}}</h6>
+              </b-jumbotron>
               <form @submit.prevent>
                 <b-button variant="primary" @click="refresh()">Refresh</b-button> &nbsp;
-                <b-button variant="primary" @click="getAnswer()">Answer</b-button> &nbsp;
+                <b-button variant="primary" @click="calcAnswer()">Answer</b-button> &nbsp;
               </form>
             </b-col>
           </b-row>
@@ -42,6 +42,12 @@ export default {
   },
   data() {
     return {
+      calculatorSign: '',
+      selectedCalculatorSign: "-",
+      options: [
+        { text: '+', value: '+' },
+        { text: '-', value: '-' }
+      ],
       digitsValue: 2,
       decimalValue: 2,
       num1: "",
@@ -63,15 +69,30 @@ export default {
         this.num2 = temp;
       }
 
+      this.calculatorSign = this.selectedCalculatorSign;
       this.answer = "";
     },
-    getAnswer() {
-      this.answer = Number(this.num1 - this.num2).toFixed(this.decimalValue);
+    calcAnswer() {
+      let theAnswer = this.calculatorSign  == '-' ?
+                          Number(this.num1) - Number(this.num2) :
+                          Number(this.num1) + Number(this.num2)
+      this.answer = theAnswer.toFixed(this.decimalValue);
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+  .col {
+    margin-bottom: 0.5rem;
+  }
 
+  .btn {
+    margin-right: 1rem;
+  }
+
+  .jumbotron {
+    margin-top: 0.5rem;
+    margin-bottom: 0.5rem;
+  }
 </style>
