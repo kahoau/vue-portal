7094817cf24d02bfa4daa2e7681f25ee2c83f01c
kahoau <kahoa827@gmail.com>
2021-09-13 09:39:32 +0800

    vue meta for countdown

--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ import router from './router'
 import store from './store'
 import './assets/scss/app.scss'
 import {BootstrapVue,IconsPlugin} from 'bootstrap-vue'
+import VueMeta from 'vue-meta'
 
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.use(VueMeta)
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
